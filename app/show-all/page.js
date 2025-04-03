03ec17cc1fead786f47957415f23eee33811a71f
AryanDhamale <aryandhamale07@gmail.com>
2025-04-03 14:36:18 +0530

    changed

--- app/show-all/page.js
+++ app/show-all/page.js
@@ -5,10 +5,9 @@ import { getallListing } from "@/serverAction/action";
 import { useEffect, useState } from "react";
 import Loading from "@/components/loader/CustomeLoading";
 
-
 function All() {
   const [Listings, setListings] = useState([]);
-  const [loader,setLoader]=useState(true);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
@@ -24,18 +23,24 @@ function All() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Ensure this code runs only on the client side
+    if (typeof document !== "undefined") {
+      console.log("Document is accessible:", document.title);
+      // Add any document-related logic here
+    }
+  }, []);
 
-  if(loader)
-  {
-    return <Loading/>  ;
+  if (loader) {
+    return <Loading />;
   }
   return (
     <div className="mt-20 pb-8 w-full mx-auto">
       <Filter />
       <div className="w-full md:w-[90%] mx-auto px-6 py-6 flex justify-start items-center flex-wrap gap-y-6 gap-x-12">
-        {
-          Listings.map((ele,idx)=><PackageCard key={idx} data={ele}/>)
-        }
+        {Listings.map((ele, idx) => (
+          <PackageCard key={idx} data={ele} />
+        ))}
       </div>
     </div>
   );
