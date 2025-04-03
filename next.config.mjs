c2aa4f0e25b2278114e379712be63e398473328c
AryanDhamale <aryandhamale07@gmail.com>
2025-04-03 13:19:56 +0530

    confing changed

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
           },
         ],
       },
+      webpack: (config) => {
+        config.module.rules.push({
+          test: /\.json$/,
+          type: 'json'
+        });
+        return config;
+      }
 };
 
 export default nextConfig;
